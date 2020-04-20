92c5a7a7655dbf1af5e8d8f986094d9590a67d62
David Stansby <dstansby@gmail.com>
2020-04-20 13:42:43 +0100

    Warn odd values

--- linalg.py
+++ linalg.py
@@ -3,6 +3,8 @@ import logging
 import numpy as np
 import astropy.units as u
 
+logger = logging.getLogger(__name__)
+
 
 def gsvd(A, B):
     # Generalised Singular Value Decomposition
@@ -11,6 +13,10 @@ def gsvd(A, B):
 
     beta = 1 / np.sqrt(1 + s**2)
     alpha = s * beta
+    if np.any(np.isclose(alpha, 1, rtol=0, atol=1e-8)):
+        logger.warn('Some singular value values very close to 1')
+    if np.any(np.isclose(beta, 1, rtol=0, atol=1e-8)):
+        logger.warn('Some singular value values very close to 1')
 
     wT = np.linalg.inv(np.diag(alpha)) @ u.T @ A
     wT /= wT.unit **2
@@ -73,4 +79,6 @@ def inv_reg_param(sva, svb, u, w, xi0, data, K, err, reg_tweak):
     norm = np.sum(terms**2, axis=1)
     tomin = norm - reg_tweak * data.size
     muidx = np.argmin(np.abs(tomin))
+    if muidx == 0:
+        logger.warn('Selected smallest value of mu')
     return mus[muidx], dem_guess[:, muidx]
